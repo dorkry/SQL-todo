b18a5eabcd05dd06ac65f9f81249f1a1be74c39d
dorkry <79543952+dorkry@users.noreply.github.com>
2021-05-09 16:52:12 +0200

    Add files via upload
    CRUD

--- app.py
+++ app.py
@@ -46,7 +46,23 @@ def find_out():
     print(new_list)
     return render_template("find.html", result = new_list)
 
-@app.route('/delete', methods=['GET'])
+@app.route('/find_id', methods=['GET'])
+def find_id():   
+    return render_template("update.html", id_list = funkcje.find_ids())
+
+@app.route('/find_id', methods=['POST'])
+def find_id_post():
+    book = funkcje.get_book(request.form.get("id"))
+    return render_template("update.html", id_list = funkcje.find_ids(), author = book["author"], title = book["title"], format = book["format"], descr = book["descr"], id = request.form.get("id"))
+
+@app.route("/update", methods = ['POST'])
+def update():
+    print(request.form.get("id"))
+    funkcje.update(request.form.get("id"), request.form.get("author"), request.form.get("title"), request.form.get("format"), request.form.get("descr"))
+    return render_template("update.html")
+
+
+@app.route('/delete', methods =['GET'])
 def delete():
     return render_template("delete.html")
 
--- funkcje.py
+++ funkcje.py
@@ -5,7 +5,6 @@ def save(author, title, descr, format):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     c.execute("INSERT INTO lista(author, title, descr, format) VALUES (?, ?, ?, ?)", (author, title, descr, format))
-
     conn.commit()
     c.close()
     
@@ -27,3 +26,36 @@ def remove(aut, tit):
     conn.commit()
     c.close()
     return
+
+def find_ids():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("SELECT * FROM lista")
+    rows = c.fetchall()
+    conn.commit()
+    c.close()
+    id_list = []
+    for i in rows:
+        id_list.append(i['id'])
+    return id_list
+
+def get_book(id):
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("SELECT * FROM lista WHERE id = ?", (id,))
+    book = c.fetchall()
+    conn.commit()
+    c.close()
+    return book[0]
+
+def update(id, author, title, format, descr):
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("UPDATE lista SET author = ? , title = ? , format = ? , descr = ? WHERE id = ?", (author, title, format, descr, id,))
+    conn.commit()
+    c.close()
+    return
+
